ca54d7fd9243e0bf395739dd4f61f6f5c58917b9
Gabriel Vieira <115424007+nordffa@users.noreply.github.com>
2023-11-11 17:53:58 -0300

    11/11/2023

--- app.py
+++ app.py
@@ -18,6 +18,12 @@ def gerarSenha(tamanho):
     return senha_gerada
 
 
+def multiplasSenhas(qtd_senhas, qtd_caracteres):
+    for _ in range(1, qtd_senhas + 1):
+        senha = gerarSenha(qtd_caracteres)
+        print(f"Senha {_}: {senha}")
+
+
 def limpaTela():
     os.system("cls")
 
@@ -30,7 +36,13 @@ def pulaLinha():
 limpaTela()
 print("Gerador de Senhas")
 pulaLinha()
-opcoes = {1: "Básica", 2: "Intermediária", 3: "Recomendada", 4: "Personalizada"}
+opcoes = {
+    1: "Básica",
+    2: "Intermediária",
+    3: "Recomendada",
+    4: "Tamanho Personalizado",
+    5: "Múltiplas Senhas",
+}
 for k, v in opcoes.items():
     print(f"{k}: {v}")
 pulaLinha()
@@ -43,19 +55,31 @@ match escolha:
     case 1:
         senha = gerarSenha(6)  # senha de 6 digitos
         print(f"Senha básica: {senha}")
+        pyperclip.copy(senha)
+        print("Sua senha foi copiada para o clipboard.")
     case 2:
         senha = gerarSenha(8)  # senha de 8 digitos
         print(f"Senha intermediária: {senha}")
+        pyperclip.copy(senha)
+        print("Sua senha foi copiada para o clipboard.")
     case 3:
         senha = gerarSenha(12)  # senha de 12 digitos
         print(f"Senha recomendada: {senha}")
+        pyperclip.copy(senha)
+        print("Sua senha foi copiada para o clipboard.")
     case 4:
         tamanho = int(input("Qual tamanho sua senha deve ter? "))
-        senha = gerarSenha(tamanho)  # senha personalizada pelo usuário
+        senha = gerarSenha(tamanho)  # tamanho personalizado pelo usuário
         print(f"Senha personalizada de {tamanho} digitos: {senha}")
+        pyperclip.copy(senha)
+        print("Sua senha foi copiada para o clipboard.")
+    case 5:
+        qtd_senhas = int(input("Quantas senhas quer gerar? "))
+        limpaTela()
+        tamanho = int(input("Quantos caracteres deve ter as senhas? "))
+        multiplasSenhas(qtd_senhas, tamanho)
+        limpaTela()
 pulaLinha()
-pyperclip.copy(senha)
-print("Sua senha foi copiada para o clipboard.")
 
 
 # o maximo de caracteres que uma senha pode ter no modelo atual sao 70 caracteres, preciso tratar o erro
